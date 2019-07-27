73c351149c8676b1bfb8284f1bcea9bbd63138ab
Luke Yue <lukedyue@gmail.com>
2019-07-27 14:29:18 +0800

    Revert "SystemUI: Animate the keyguard when charging"
    This reverts commit 3ab33114ad64a5401379fcbb4dbbbd44e509ca7d.

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -52,7 +52,6 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-slices-builders \
     android-arch-core-runtime \
     android-arch-lifecycle-extensions \
-    frameworks-support-lottie
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     SystemUI-tags \
